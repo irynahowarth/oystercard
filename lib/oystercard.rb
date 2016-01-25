7db1aec43308626930315035f16da9023646e0a1
Iryna Howarth <irynahowarth@gmail.com>
2016-01-25 17:30:14 +0000

    add deduct functionality to oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,18 @@
 class Oystercard
   attr_reader :balance
 
+  MAX_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up amount
+    fail "Balance will exceed maximum #{MAX_LIMIT}!" if (@balance + amount) > MAX_LIMIT
     @balance += amount
+  end
+
+  def deduct amount
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,15 @@ describe Oystercard do
     expect(subject.balance).to eq 5
   end
 
+  it "should raise error if on top_up balance will exceed 90" do
+    message = "Balance will exceed maximum #{Oystercard::MAX_LIMIT}!"
+    expect{subject.top_up(Oystercard::MAX_LIMIT+1)}.to raise_error message
+  end
+
+  it "should deduct balance on 20 when #deduct(20)" do
+    subject.top_up(30)
+    subject.deduct(20)
+    expect(subject.balance).to eq 10
+  end
+
 end
