e8b06a65388acfa3f0a4b9f14f7347da2e51517f
Iryna Howarth <irynahowarth@gmail.com>
2016-01-25 16:51:58 +0000

    add top_up functionality for oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
 require 'oystercard'
+
 describe Oystercard do
+
+  it "on initialize should have balance 0" do
+    expect(subject.balance).to eq 0
+  end
+
+  it "should change balance to 5 when #topup(5)" do
+    subject.top_up(5)
+    expect(subject.balance).to eq 5
+  end
+
 end
