b9bd77dcc1e3a912c019e85179fc8ce17ac9d4a1
Iryna Howarth <irynahowarth@gmail.com>
2016-01-27 12:24:48 +0000

    made class Station into Struct

--- lib/station.rb
+++ lib/station.rb
@@ -1,7 +1,8 @@
-class Station
-  attr_reader :name, :zone
-  def initialize(name, zone)
-    @name = name
-    @zone = zone
-  end
+class Station < Struct.new :name, :zone
+
 end
+
+# station_list = [{'london',1},{'paddington',2},{'kingth cross',3}]
+
+# station = station_list.find{|item| item[:name]=='london' }
+# station[:zone]
