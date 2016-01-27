29a411ea4c3536e7220507e2d29a0007135cfa7e
Mark Hill <futonfutonda@gmail.com>
2016-01-27 11:43:52 +0000

    created a station class ch13

---
+++ spec/station_spec.rb
@@ -0,0 +1,13 @@
+require 'station'
+describe Station do
+  subject(:station){described_class.new "London", 4}
+
+  describe '#initialize'do
+    it 'should have a name'do
+      expect(subject.name).to eq "London"
+    end
+    it 'should have a zone'do
+      expect(subject.zone).to eq 4
+    end
+  end
+end
